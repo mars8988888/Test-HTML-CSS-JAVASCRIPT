521d5598e4a92c30bb26d3afc3f2ad5f252c6352
mars8988888 <72305384+mars@users.noreply.github.com>
2020-11-03 23:02:49 +0700

    checck

--- bai thi/RGU.js
+++ bai thi/RGU.js
@@ -1,17 +1,35 @@
 function check(){
-        var reUsername = /^[A-Za-z0-9]+$/;
+        var reUsername = /^\w{6,12}$/;
             var sUsername = reg.txtUsername.value;
             if (!reUsername.test(sUsername)) {
-                alert("Username is invalid");
+                alert("Username cant be blank and must 6 to 12 characters");
                 reg.txtUsername.focus();
                 return false;  
             }
-        srePassword =reg.txtrePassword.value;
-            if(sPass != srePassword){
-                alert("Confirm password doest not match");
-                reg.txtrePassword.focus();
+        var rePassword = /^\w{6,12}$/;
+            var sPassword = reg.txtPassword.value;
+            if (!rePassword.test(sPassword)) {
+                alert("Password cant be blank and must 6 to 12 characters");
+                reg.txtPassword.focus();
+                return false;  
+            } else if (sPassword == sUsername){
+                alert("Password cant be the same with username");
+                reg.txtPassword.focus();
                 return false;
+            }
+        
+        var srePassword = /^\w{6,12}$/;
+        var Cpassword =reg.txtsPassword.value;
+            if(!srePassword.test(Cpassword)){
+                alert("You must enter confirm password");
+                reg.txtsPassword.focus();
+                return false;
+        } else if (Cpassword != sPassword){
+            alert("Your confirm password doesnt match ");
+            reg.txtsPassword.focus();
+            return false;
         }
+
         var reFirstName = /^[A-Za-z0-9]+$/;
         var sFirstName = reg.txtFirstName.value;
         if (!reFirstName.test(sFirstName)) {
@@ -33,14 +51,22 @@ function check(){
                 reg.txtEmail.focus();
                 return false;
             }
+        var reMobile = /^[0-9]+$/;
+            var sMobile = reg.txtMobile.value;
+                if (!reMobile.test(sMobile)) {
+                    alert("Mobile is invalid");
+                    reg.txtMobile.focus();
+                    return false;
+                }  
+                  
         var reTelephone = /^[0-9]+$/;
-        var sTelephone = reg.txtPhone.value;
+        var sTelephone = reg.txtTelephone.value;
             if (!reTelephone.test(sTelephone)) {
                 alert("Telephone is invalid");
-                reg.txtPhone.focus();
+                reg.txtTelephone.focus();
                 return false;
             }
         
-        alert("Congratulation!! Your information has been sent successfully")
+        alert("Congratulation!! Your account has been created successfully")
         return true;
 }
